7791d0d9fbc21d73d1494129345990e8177f85f9
wahyualdika <36392359+wahyualdika@users.noreply.github.com>
2018-02-12 10:34:53 +0700

    Update adminMahasiswaForm.blade.php

--- adminMahasiswaForm.blade.php
+++ adminMahasiswaForm.blade.php
@@ -17,8 +17,6 @@
 
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
-    <script src="https://cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/src/js/bootstrap-datetimepicker.js"></script>
     <script type="text/javascript">
         $( function() {
             $( "#datepicker" ).datepicker();
